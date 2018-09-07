f63637304d8b86dbc3afbd6a7279b7ba166fbf66
Justin Fyfe <justin.fyfe1@mohawkcollege.ca>
2018-09-07 16:08:28 -0400

    Added SQL Data persistence service which can execute raw sql;

---
+++ SanteDB.Core.Api/Services/ISqlDataPersistenceService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SanteDB.Core.Services
+{
+    /// <summary>
+    /// Represents a data persistence service where arbitrary SQL can be run
+    /// </summary>
+    public interface ISqlDataPersistenceService
+    {
+
+        /// <summary>
+        /// Text that identifies the type of database system that is running
+        /// </summary>
+        string InvariantName { get; }
+
+        /// <summary>
+        /// Executes the arbitrary SQL
+        /// </summary>
+        void Execute(String sql);
+    }
+}
