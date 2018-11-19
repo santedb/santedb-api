585b68d1fd1a14c838b003e4c2164984b0f963d7
Justin Fyfe <justin.fyfe1@mohawkcollege.ca>
2018-11-19 15:53:42 +0630

    Added more data to the security information service

---
+++ SanteDB.Core.Api/Security/ISecurityInformationService.cs
@@ -0,0 +1,37 @@
+﻿using SanteDB.Core.Model.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SanteDB.Core.Security
+{
+    /// <summary>
+	/// Represents a contract for a wrapper service to allow interoperation between the 
+    /// 
+	/// </summary>
+	public interface ISecurityInformationService
+    {
+        /// Get active policies for the specified securable type
+        /// </summary>
+        IEnumerable<SecurityPolicyInstance> GetActivePolicies(object securable);
+
+        /// <summary>
+        /// Change password of a principal
+        /// </summary>
+        void ChangePassword(String userName, String password, IPrincipal principal);
+
+        /// <summary>
+        /// Add users to roles
+        /// </summary>
+        void AddUsersToRoles(String[] users, String[] roles);
+
+        /// <summary>
+        /// Remove users from roles
+        /// </summary>
+        void RemoveUsersFromRoles(String[] users, String[] roles);
+    }
+
+}
