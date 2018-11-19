d8c7467e1da7aedd1c2a6ef886dd4800eafc90ea
Justin Fyfe <justin.fyfe1@mohawkcollege.ca>
2018-11-19 15:43:31 +0630

    Added policy metadata service

---
+++ SanteDB.Core.Api/Security/IPolicyMetadataService.cs
@@ -0,0 +1,21 @@
+﻿using SanteDB.Core.Model.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SanteDB.Core.Security
+{
+    /// <summary>
+	/// Represents a contract for a policy information service
+	/// </summary>
+	public interface IPolicyMetadataService
+    {
+        /// Get active policies for the specified securable type
+        /// </summary>
+        IEnumerable<SecurityPolicyInstance> GetActivePolicies(object securable);
+
+    }
+
+}
