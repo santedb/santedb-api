968d25e8f20856b0b9a8ffcbe1ed0dbfd8482841
Justin Fyfe <justin.fyfe1@mohawkcollege.ca>
2018-11-27 09:11:45 +0630

    Moved entity sources to this project to reuse more easily

---
+++ SanteDB.Core.Api/Data/PersistenceEntitySource.cs
@@ -0,0 +1,106 @@
+﻿/*
+ * Copyright 2015-2018 Mohawk College of Applied Arts and Technology
+ *
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you 
+ * may not use this file except in compliance with the License. You may 
+ * obtain a copy of the License at 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
+ * License for the specific language governing permissions and limitations under 
+ * the License.
+ * 
+ * User: justin
+ * Date: 2018-6-28
+ */
+using SanteDB.Core.Model;
+using SanteDB.Core.Model.Acts;
+using SanteDB.Core.Model.EntityLoader;
+using SanteDB.Core.Model.Interfaces;
+using SanteDB.Core.Security;
+using SanteDB.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace SanteDB.Core.Data
+{
+    /// <summary>
+    /// Entity source which loads local objects
+    /// </summary>
+    public class PersistenceEntitySource : IEntitySourceProvider
+    {
+
+
+        #region IEntitySourceProvider implementation
+
+        /// <summary>
+        /// Get the persistence service source
+        /// </summary>
+        public TObject Get<TObject>(Guid? key) where TObject : IdentifiedData, new()
+        {
+            var persistenceService = ApplicationServiceContext.Current.GetService<IDataPersistenceService<TObject>>();
+            if (persistenceService != null && key.HasValue)
+                return persistenceService.Get(key.Value, null, false, AuthenticationContext.Current.Principal);
+            return default(TObject);
+        }
+
+        /// <summary>
+        /// Get the specified version
+        /// </summary>
+        public TObject Get<TObject>(Guid? key, Guid? versionKey) where TObject : IdentifiedData, IVersionedEntity, new()
+        {
+            var persistenceService = ApplicationServiceContext.Current.GetService<IDataPersistenceService<TObject>>();
+            if (persistenceService != null && key.HasValue)
+                return persistenceService.Query(o => o.Key == key, AuthenticationContext.SystemPrincipal).FirstOrDefault();
+            else if (persistenceService != null && key.HasValue && versionKey.HasValue)
+                return persistenceService.Query(o => o.Key == key && o.VersionKey == versionKey, AuthenticationContext.Current.Principal).FirstOrDefault();
+            return default(TObject);
+        }
+
+        /// <summary>
+        /// Get versioned relationships for the object
+        /// </summary>
+        public IEnumerable<TObject> GetRelations<TObject>(Guid? sourceKey, decimal? sourceVersionSequence) where TObject : IdentifiedData, IVersionedAssociation, new()
+        {
+            return this.Query<TObject>(o => o.SourceEntityKey == sourceKey).ToList();
+        }
+
+        /// <summary>
+        /// Get versioned relationships for the object
+        /// </summary>
+        public IEnumerable<TObject> GetRelations<TObject>(Guid? sourceKey) where TObject : IdentifiedData, ISimpleAssociation, new()
+        {
+            return this.Query<TObject>(o => o.SourceEntityKey == sourceKey).ToList();
+        }
+
+        /// <summary>
+        /// Query the specified object
+        /// </summary>
+        public IEnumerable<TObject> Query<TObject>(Expression<Func<TObject, bool>> query) where TObject : IdentifiedData, new()
+        {
+            var persistenceService = ApplicationServiceContext.Current.GetService<IFastQueryDataPersistenceService<TObject>>();
+            if (persistenceService != null)
+            {
+                var tr = 0;
+                if (typeof(Act).GetTypeInfo().IsAssignableFrom(typeof(TObject).GetTypeInfo()) ||
+                    typeof(ActParticipation).GetTypeInfo().IsAssignableFrom(typeof(TObject).GetTypeInfo()))
+                    return persistenceService.QueryFast(query, Guid.Empty, 0, null, out tr, AuthenticationContext.Current.Principal);
+                else
+                    return persistenceService.Query(query, Guid.Empty, 0, null, out tr, AuthenticationContext.Current.Principal);
+
+            }
+            return new List<TObject>();
+        }
+
+        #endregion
+
+    }
+}
+
---
+++ SanteDB.Core.Api/Data/RepositoryEntitySource.cs
@@ -0,0 +1,99 @@
+﻿/*
+ * Copyright 2015-2018 Mohawk College of Applied Arts and Technology
+ *
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you 
+ * may not use this file except in compliance with the License. You may 
+ * obtain a copy of the License at 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
+ * License for the specific language governing permissions and limitations under 
+ * the License.
+ * 
+ * User: justin
+ * Date: 2018-11-23
+ */
+using SanteDB.Core.Model;
+using SanteDB.Core.Model.EntityLoader;
+using SanteDB.Core.Model.Interfaces;
+using SanteDB.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace SanteDB.Core.Data
+{
+    /// <summary>
+    /// Entity source which loads objects using the IRepositoryService<> instead of IDataPersistenceService<>
+    /// </summary>
+    public class RepositoryEntitySource : IEntitySourceProvider
+    {
+
+
+        #region IEntitySourceProvider implementation
+
+        /// <summary>
+        /// Get the persistence service source
+        /// </summary>
+        public TObject Get<TObject>(Guid? key) where TObject : IdentifiedData, new()
+        {
+            var persistenceService = ApplicationServiceContext.Current.GetService<IRepositoryService<TObject>>();
+            if (persistenceService != null && key.HasValue)
+                return persistenceService.Get(key.Value);
+            return default(TObject);
+        }
+
+        /// <summary>
+        /// Get the specified version
+        /// </summary>
+        public TObject Get<TObject>(Guid? key, Guid? versionKey) where TObject : IdentifiedData, IVersionedEntity, new()
+        {
+            var persistenceService = ApplicationServiceContext.Current.GetService<IRepositoryService<TObject>>();
+            if (persistenceService != null && key.HasValue)
+                return persistenceService.Find(o => o.Key == key).FirstOrDefault();
+            else if (persistenceService != null && key.HasValue && versionKey.HasValue)
+                return persistenceService.Find(o => o.Key == key && o.VersionKey == versionKey).FirstOrDefault();
+            return default(TObject);
+        }
+
+        /// <summary>
+        /// Get versioned relationships for the object
+        /// </summary>
+        public IEnumerable<TObject> GetRelations<TObject>(Guid? sourceKey, decimal? sourceVersionSequence) where TObject : IdentifiedData, IVersionedAssociation, new()
+        {
+            return this.Query<TObject>(o => o.SourceEntityKey == sourceKey).ToList();
+        }
+
+        /// <summary>
+        /// Get versioned relationships for the object
+        /// </summary>
+        public IEnumerable<TObject> GetRelations<TObject>(Guid? sourceKey) where TObject : IdentifiedData, ISimpleAssociation, new()
+        {
+            return this.Query<TObject>(o => o.SourceEntityKey == sourceKey).ToList();
+        }
+
+        /// <summary>
+        /// Query the specified object
+        /// </summary>
+        public IEnumerable<TObject> Query<TObject>(Expression<Func<TObject, bool>> query) where TObject : IdentifiedData, new()
+        {
+            var persistenceService = ApplicationServiceContext.Current.GetService<IRepositoryService<TObject>>();
+            if (persistenceService != null)
+            {
+                var tr = 0;
+                return persistenceService.Find(query, 0, null, out tr);
+
+            }
+            return new List<TObject>();
+        }
+
+        #endregion
+
+    }
+}
+
