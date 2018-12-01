44fae2bc6ee6a5fb0719b22799fbce6c41a77558
Justin Fyfe <justin.fyfe1@mohawkcollege.ca>
2018-12-01 13:59:58 +0630

    Updated version info

--- SanteDB.Core.Api/Properties/AssemblyInfo.cs
+++ SanteDB.Core.Api/Properties/AssemblyInfo.cs
@@ -42,7 +42,7 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.9.0.*")][assembly: AssemblyInformationalVersion("Gananoque")]
-[assembly: AssemblyVersion("1.9.1.*")]
+// [assembly: AssemblyVersion("1.9.14.*")][assembly: AssemblyInformationalVersion("Gananoque")]
+[assembly: AssemblyVersion("1.9.14.*")]
 [assembly: AssemblyInformationalVersion("Gananoque")]
 [assembly: AssemblyFileVersion("1.9.1.0")]
