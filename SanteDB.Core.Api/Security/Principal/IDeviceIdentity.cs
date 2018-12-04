788e19d3df2905bc31e6f730055dcdc83400b556
Justin Fyfe <justin.fyfe1@mohawkcollege.ca>
2018-12-04 21:29:39 +0700

    Updated to have IDeviceIdentity interface

---
+++ SanteDB.Core.Api/Security/Principal/IDeviceIdentity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SanteDB.Core.Security.Principal
+{
+    /// <summary>
+    /// Represents a device identity
+    /// </summary>
+    public interface IDeviceIdentity : IIdentity
+    {
+    }
+}
