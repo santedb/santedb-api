91710496062f2dc97c57bfa6a98741fac7f50c95
Justin Fyfe <justin@fyfesoftware.ca>
2019-03-16 11:34:27 -0400

    Rev'd versions

--- SanteDB.Core.Api/Properties/AssemblyInfo.cs
+++ SanteDB.Core.Api/Properties/AssemblyInfo.cs
@@ -42,7 +42,7 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.28.0.*")][assembly: AssemblyInformationalVersion("Halifax")]
-[assembly: AssemblyVersion("1.28.0.*")]
+// [assembly: AssemblyVersion("1.30.0.*")][assembly: AssemblyInformationalVersion("Halifax")]
+[assembly: AssemblyVersion("1.30.0.*")]
 [assembly: AssemblyInformationalVersion("Halifax")]
-[assembly: AssemblyFileVersion("1.28.0.0")]
+[assembly: AssemblyFileVersion("1.30.0.0")]
