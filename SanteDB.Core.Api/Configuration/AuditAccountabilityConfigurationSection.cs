4452365ad0f0a10f6f061f9c066c3e11dbbcee2b
Justin Fyfe <justin@fyfesoftware.ca>
2019-12-25 10:24:37 +0630

    Added ability to filter audits on the auditing side

---
+++ SanteDB.Core.Api/Configuration/AuditAccountabilityConfigurationSection.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace SanteDB.Core.Configuration
+{
+    /// <summary>
+    /// Confiugration related to auditing and accountability
+    /// </summary>
+    [XmlType(nameof(AuditAccountabilityConfigurationSection), Namespace = "http://santedb.org/configuration")]
+    public class AuditAccountabilityConfigurationSection : IConfigurationSection
+    {
+
+        /// <summary>
+        /// When set to true, enables complete audit trail
+        /// </summary>
+        [XmlAttribute("completeAuditTrail"), JsonProperty("completeAuditTrail")]
+        public bool CompleteAuditTrail { get; set; }
+
+        /// <summary>
+        /// Gets or sets filters to apply to the audit trail (i.e. ignore)
+        /// </summary>
+        [XmlArray("filters"), XmlArrayItem("add"), JsonProperty("filters")]
+        public List<AuditFilterConfiguration> AuditFilters { get; set; }
+    }
+}
---
+++ SanteDB.Core.Api/Configuration/AuditFilterConfiguration.cs
@@ -0,0 +1,76 @@
+﻿using Newtonsoft.Json;
+using System.Xml.Serialization;
+
+namespace SanteDB.Core.Configuration
+{
+    /// <summary>
+    /// Audit filter configuration
+    /// </summary>
+    [XmlType(nameof(AuditFilterConfiguration), Namespace = "http://santedb.org/configuration")]
+    public class AuditFilterConfiguration
+    {
+
+        /// <summary>
+        /// Default ctor for serialization
+        /// </summary>
+        public AuditFilterConfiguration()
+        {
+
+        }
+
+        /// <summary>
+        /// Creates a new audit filter configuration object
+        /// </summary>
+        public AuditFilterConfiguration(Auditing.ActionType? action, Auditing.EventIdentifierType? eventType, Auditing.OutcomeIndicator? outcomeType, bool insertLocal, bool sendRemote)
+        {
+            this.Action = action.GetValueOrDefault();
+            this.Event = eventType.GetValueOrDefault();
+            this.Outcome = outcomeType.GetValueOrDefault();
+            this.ActionSpecified = action.HasValue;
+            this.EventSpecified = eventType.HasValue;
+            this.OutcomeSpecified = outcomeType.HasValue;
+            this.InsertLocal = insertLocal;
+            this.SendRemote = sendRemote;
+        }
+
+        /// <summary>
+        /// Filter on action type
+        /// </summary>
+        [XmlAttribute("action"), JsonProperty("action")]
+        public Auditing.ActionType Action { get; set; }
+
+        /// <summary>
+        /// Filter on event
+        /// </summary>
+        [XmlAttribute("event"), JsonProperty("event")]
+        public Auditing.EventIdentifierType Event { get; set; }
+
+        /// <summary>
+        /// Filter on outcome
+        /// </summary>
+        [XmlAttribute("outcome"), JsonProperty("outcome")]
+        public Auditing.OutcomeIndicator Outcome { get; set; }
+
+        /// <summary>
+        /// True if when a filter matches the audit you want to include locally
+        /// </summary>
+        [XmlAttribute("insert"), JsonProperty("insert")]
+        public bool InsertLocal { get; set; }
+
+        /// <summary>
+        /// True when filter is active to shipt
+        /// </summary>
+        [XmlAttribute("ship"), JsonProperty("ship")]
+        public bool SendRemote { get; set; }
+
+        #region Serialization Control
+        [XmlIgnore,JsonIgnore]
+        public bool ActionSpecified { get; set; }
+        [XmlIgnore,JsonIgnore]
+        public bool EventSpecified { get; set; }
+        [XmlIgnore,JsonIgnore]
+        public bool OutcomeSpecified { get; set; }
+        #endregion
+
+    }
+}
