337087025bb2246d4accc797e01ecdab184007e8
Justin Fyfe <justin@fyfesoftware.ca>
2020-01-07 12:07:23 +0630

    Rev'd versions

--- SanteDB.Core.Api/Properties/AssemblyInfo.cs
+++ SanteDB.Core.Api/Properties/AssemblyInfo.cs
@@ -42,7 +42,7 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.114.0.*")][assembly: AssemblyInformationalVersion("Iqaluit")]
-[assembly: AssemblyVersion("1.114.0.*")]
+// [assembly: AssemblyVersion("1.115.0.*")][assembly: AssemblyInformationalVersion("Iqaluit")]
+[assembly: AssemblyVersion("1.115.0.*")]
 [assembly: AssemblyInformationalVersion("Iqaluit")]
-[assembly: AssemblyFileVersion("1.114.0.0")]
+[assembly: AssemblyFileVersion("1.115.0.0")]
