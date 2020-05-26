26aaf4f8a40ed6ab16b32dd8791600649e899de6
Justin Fyfe <justin@fyfesoftware.ca>
2020-05-26 10:39:41 -0400

    Updated backup service to run automatically

--- SanteDB.Core.Api/Configuration/Data/DataConfigurationSection.cs
+++ SanteDB.Core.Api/Configuration/Data/DataConfigurationSection.cs
@@ -66,7 +66,7 @@ namespace SanteDB.Core.Configuration.Data
         /// </summary>
         public ConnectionString()
         {
-
+            this.Value = String.Empty;
         }
 
         /// <summary>
