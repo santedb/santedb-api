d06280f35bcc62f0a9635a4a227a449b81d8190f
Justin Fyfe <justin@fyfesoftware.ca>
2020-06-03 03:01:12 -0400

    Added resource locking service to prevent concurrent editing

---
+++ SanteDB.Core.Api/Services/IResourceEditLockService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SanteDB.Core.Api.Services
+{
+    /// <summary>
+    /// A resource locking service 
+    /// </summary>
+    public interface IResourceEditLockService
+    {
+
+        /// <summary>
+        /// Try to get a lock on the resource for editing
+        /// </summary>
+        bool Lock<T>(Guid key);
+
+        /// <summary>
+        /// Release the lock on the specified key
+        /// </summary>
+        bool Unlock<T>(Guid key);
+    }
+}
