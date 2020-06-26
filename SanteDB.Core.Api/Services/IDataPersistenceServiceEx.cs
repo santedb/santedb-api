9d77e0db1099375a5b0b801eacbe4dbc8c9cf146
Justin Fyfe <justin@fyfesoftware.ca>
2020-06-26 14:42:36 -0400

    Removed Everest Dependencies

---
+++ SanteDB.Core.Api/Services/IDataPersistenceServiceEx.cs
@@ -0,0 +1,22 @@
+﻿using SanteDB.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Security.Principal;
+using System.Text;
+
+namespace SanteDB.Core.Services
+{
+    /// <summary>
+    /// Extended data persistence service
+    /// </summary>
+    public interface IDataPersistenceServiceEx<TModel> : IDataPersistenceService<TModel>
+        where TModel : IdentifiedData
+    {
+
+        /// <summary>
+        /// Touch the specified data
+        /// </summary>
+        void Touch(Guid key, TransactionMode mode, IPrincipal principal);
+
+    }
+}
