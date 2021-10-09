d5da7bd69491b9d2b2fa1dd7f363f6293b442e7c
Justin Fyfe <justin@fyfesoftware.ca>
2021-10-09 12:07:30 -0400

    Cleaned up the ctor for detected issue

--- SanteDB.Core.Api/Exceptions/DetectedIssueException.cs
+++ SanteDB.Core.Api/Exceptions/DetectedIssueException.cs
@@ -2,22 +2,23 @@
  * Copyright (C) 2021 - 2021, SanteSuite Inc. and the SanteSuite Contributors (See NOTICE.md for full copyright notices)
  * Copyright (C) 2019 - 2021, Fyfe Software Inc. and the SanteSuite Contributors
  * Portions Copyright (C) 2015-2018 Mohawk College of Applied Arts and Technology
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you 
- * may not use this file except in compliance with the License. You may 
- * obtain a copy of the License at 
- * 
- * http://www.apache.org/licenses/LICENSE-2.0 
- * 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
- * License for the specific language governing permissions and limitations under 
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
  * the License.
- * 
+ *
  * User: fyfej
  * Date: 2021-8-5
  */
+
 using SanteDB.Core.BusinessRules;
 using SanteDB.Core.Services;
 using System;
@@ -31,32 +32,29 @@ namespace SanteDB.Core.Exceptions
     /// </summary>
     public class DetectedIssueException : Exception
     {
-
         /// <summary>
-        /// Gets the list of issues set by the BRE 
+        /// Gets the list of issues set by the BRE
         /// </summary>
-        public List<DetectedIssue> Issues { get; private set; }
+        public IEnumerable<DetectedIssue> Issues { get; private set; }
 
         /// <summary>
         /// Creates a new detected issue exception
         /// </summary>
         public DetectedIssueException()
         {
-
         }
 
         /// <summary>
-        /// Creates a new detected issue exception with the specified <paramref name="issues"/> 
+        /// Creates a new detected issue exception with the specified <paramref name="issues"/>
         /// </summary>
-        public DetectedIssueException(List<DetectedIssue> issues, Exception cause) : this(issues, null, cause)
+        public DetectedIssueException(IEnumerable<DetectedIssue> issues, Exception cause) : this(issues, null, cause)
         {
-
         }
 
         /// <summary>
         /// Creates a new detected issue exception with the specified <paramref name="issues"/> <paramref name="message"/> and causal exception (<paramref name="innerException"/>)
         /// </summary>
-        public DetectedIssueException(List<DetectedIssue> issues, String message, Exception innerException) : base(message, innerException)
+        public DetectedIssueException(IEnumerable<DetectedIssue> issues, String message, Exception innerException) : base(message, innerException)
         {
             this.Issues = issues;
         }
@@ -64,16 +62,15 @@ namespace SanteDB.Core.Exceptions
         /// <summary>
         /// Creates a new detected issue exception with the specified issue list
         /// </summary>
-        public DetectedIssueException(List<DetectedIssue> issues) : this(issues, null, null)
+        public DetectedIssueException(IEnumerable<DetectedIssue> issues) : this(issues, null, null)
         {
         }
 
         /// <summary>
         /// Detected issue exception
         /// </summary>
-        public DetectedIssueException(DetectedIssue issue) : this(new List<DetectedIssue>() {  issue })
+        public DetectedIssueException(DetectedIssue issue) : this(new List<DetectedIssue>() { issue })
         {
-
         }
 
         /// <summary>
@@ -81,7 +78,6 @@ namespace SanteDB.Core.Exceptions
         /// </summary>
         public DetectedIssueException(DetectedIssue issue, Exception cause) : this(new List<DetectedIssue>() { issue }, cause)
         {
-
         }
 
         /// <summary>
