f1262f1d2aa54c1999389d013f2bbd872ca7bf18
Justin Fyfe <justin@fyfesoftware.ca>
2021-10-19 17:18:06 -0400

    Updated matching service routines to properly sticky objects - Updated caching routines to remove extended ITargetedAssociation relations

--- SanteDB.Core.Api/Services/IDataCacheService.cs
+++ SanteDB.Core.Api/Services/IDataCacheService.cs
@@ -2,28 +2,28 @@
  * Copyright (C) 2021 - 2021, SanteSuite Inc. and the SanteSuite Contributors (See NOTICE.md for full copyright notices)
  * Copyright (C) 2019 - 2021, Fyfe Software Inc. and the SanteSuite Contributors
  * Portions Copyright (C) 2015-2018 Mohawk College of Applied Arts and Technology
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you 
- * may not use this file except in compliance with the License. You may 
- * obtain a copy of the License at 
- * 
- * http://www.apache.org/licenses/LICENSE-2.0 
- * 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
- * License for the specific language governing permissions and limitations under 
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
  * the License.
- * 
+ *
  * User: fyfej
  * Date: 2021-8-5
  */
+
 using SanteDB.Core.Model;
 using System;
 
 namespace SanteDB.Core.Services
 {
-
     /// <summary>
     /// Data cache event args
     /// </summary>
@@ -54,10 +54,12 @@ namespace SanteDB.Core.Services
         /// Item was added to cache
         /// </summary>
         event EventHandler<DataCacheEventArgs> Added;
+
         /// <summary>
         /// Item was updated from cache
         /// </summary>
         event EventHandler<DataCacheEventArgs> Updated;
+
         /// <summary>
         /// Item was removed from cache
         /// </summary>
@@ -82,6 +84,11 @@ namespace SanteDB.Core.Services
         /// Removes an object from the cache
         /// </summary>
         void Remove(Guid key);
+
+        /// <summary>
+        /// Removes an object from the cache
+        /// </summary>
+        void Remove(IdentifiedData data);
 
         /// <summary>
         /// Clear all keys in the cache
