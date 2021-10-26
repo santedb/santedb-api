59462e57ff45299046f447abe860110b8f4895ff
Justin Fyfe <justin@fyfesoftware.ca>
2021-10-26 14:18:53 -0400

    Updated the pub-sub channel definition

--- SanteDB.Core.Api/PubSub/PubSubChannelDefinition.cs
+++ SanteDB.Core.Api/PubSub/PubSubChannelDefinition.cs
@@ -2,22 +2,23 @@
  * Copyright (C) 2021 - 2021, SanteSuite Inc. and the SanteSuite Contributors (See NOTICE.md for full copyright notices)
  * Copyright (C) 2019 - 2021, Fyfe Software Inc. and the SanteSuite Contributors
  * Portions Copyright (C) 2015-2018 Mohawk College of Applied Arts and Technology
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you 
- * may not use this file except in compliance with the License. You may 
- * obtain a copy of the License at 
- * 
- * http://www.apache.org/licenses/LICENSE-2.0 
- * 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
- * License for the specific language governing permissions and limitations under 
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
  * the License.
- * 
+ *
  * User: fyfej
  * Date: 2021-8-5
  */
+
 using Newtonsoft.Json;
 using SanteDB.Core.Model;
 using System;
@@ -34,7 +35,6 @@ namespace SanteDB.Core.PubSub
     [JsonObject]
     public class PubSubChannelDefinition : NonVersionedEntityData
     {
-
         /// <summary>
         /// Gets or sets the name
         /// </summary>
@@ -51,7 +51,7 @@ namespace SanteDB.Core.PubSub
         /// Gets or sets the endpoint
         /// </summary>
         [XmlElement("endpoint"), JsonProperty("endpoint")]
-        public Uri Endpoint { get; set; }
+        public String Endpoint { get; set; }
 
         /// <summary>
         /// Gets or sets the settings on this channel
@@ -70,6 +70,5 @@ namespace SanteDB.Core.PubSub
         /// </summary>
         [XmlIgnore, JsonIgnore]
         public Type DispatcherFactoryType => System.Type.GetType(this.DispatcherFactoryTypeXml);
-
     }
 }
