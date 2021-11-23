86cdadf273da5fdc78f24750fb6a29adbf77ea68
Justin Fyfe <justin@fyfesoftware.ca>
2021-11-23 12:44:00 -0500

    Updated the documentation

--- SanteDB.Core.Api/Diagnostics/Tracer.cs
+++ SanteDB.Core.Api/Diagnostics/Tracer.cs
@@ -2,26 +2,28 @@
  * Copyright (C) 2021 - 2021, SanteSuite Inc. and the SanteSuite Contributors (See NOTICE.md for full copyright notices)
  * Copyright (C) 2019 - 2021, Fyfe Software Inc. and the SanteSuite Contributors
  * Portions Copyright (C) 2015-2018 Mohawk College of Applied Arts and Technology
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you 
- * may not use this file except in compliance with the License. You may 
- * obtain a copy of the License at 
- * 
- * http://www.apache.org/licenses/LICENSE-2.0 
- * 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
- * License for the specific language governing permissions and limitations under 
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
  * the License.
- * 
+ *
  * User: fyfej
  * Date: 2021-8-5
  */
+
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Diagnostics.Tracing;
+using System.Linq;
 
 namespace SanteDB.Core.Diagnostics
 {
@@ -108,9 +110,7 @@ namespace SanteDB.Core.Diagnostics
         /// </summary>
         public void TraceUntestedWarning()
         {
-
             this.TraceEvent(EventLevel.Warning, "UNTESTED CODE WARNING ----> A PROCESS CALLED AN UNTESTED SECTION OF CODE SUBSEQUENT ERRORS MAY APPEAR IN THE LOG ----> {0}", new StackTrace(true));
-
         }
 
         /// <summary>
@@ -131,6 +131,15 @@ namespace SanteDB.Core.Diagnostics
         public void TraceVerbose(String format, params Object[] args)
         {
             this.TraceEvent(EventLevel.Verbose, format, args);
+        }
+
+        /// <summary>
+        /// Get the specified trace writer
+        /// </summary>
+        public static TWriter GetWriter<TWriter>()
+            where TWriter : TraceWriter
+        {
+            return m_writers.FirstOrDefault(o => o.Value is TWriter).Value as TWriter;
         }
     }
 }
--- SanteDB.Core.Api/Services/IServiceImplementation.cs
+++ SanteDB.Core.Api/Services/IServiceImplementation.cs
@@ -2,32 +2,35 @@
  * Copyright (C) 2021 - 2021, SanteSuite Inc. and the SanteSuite Contributors (See NOTICE.md for full copyright notices)
  * Copyright (C) 2019 - 2021, Fyfe Software Inc. and the SanteSuite Contributors
  * Portions Copyright (C) 2015-2018 Mohawk College of Applied Arts and Technology
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you 
- * may not use this file except in compliance with the License. You may 
- * obtain a copy of the License at 
- * 
- * http://www.apache.org/licenses/LICENSE-2.0 
- * 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
- * License for the specific language governing permissions and limitations under 
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
  * the License.
- * 
+ *
  * User: fyfej
  * Date: 2021-8-5
  */
+
 using System;
 
 namespace SanteDB.Core.Services
 {
     /// <summary>
-    /// Represents a marker class for a service implementation
+    /// Defines a basic marker class for all service implementations
     /// </summary>
+    /// <remarks><para>The <see cref="IServiceImplementation"/> interface is used to denote a class which implements a service. There is no
+    /// rule which would preclude plugins from defining services which do not extend this base interface, however the presence of a common
+    /// <see cref="ServiceName"/> property is useful for reflecting the current state of the application host context.</para></remarks>
     public interface IServiceImplementation
     {
-
         /// <summary>
         /// Gets the service name
         /// </summary>
