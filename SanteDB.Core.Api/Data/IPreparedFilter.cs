cd81f2b9372e086c798381f1cef7c25eec0b1d31
Justin Fyfe <justin@fyfesoftware.ca>
2022-01-20 15:39:31 -0500

    Added prepared filter definition

---
+++ SanteDB.Core.Api/Data/IPreparedFilter.cs
@@ -0,0 +1,91 @@
+﻿using SanteDB.Core.Model;
+using SanteDB.Core.Model.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace SanteDB.Core.Data
+{
+
+    /// <summary>
+    /// The status of the filter index
+    /// </summary>
+    public enum PreparedFilterIndexState
+    {
+        /// <summary>
+        /// The filter index is active and in use
+        /// </summary>
+        Active,
+        /// <summary>
+        /// The filter index is being re-built
+        /// </summary>
+        Rebuilding,
+        /// <summary>
+        /// The fiter index is inactive (not being used for filtering)
+        /// </summary>
+        Inactive
+    }
+
+    /// <summary>
+    /// Represents prepared filter metadata
+    /// </summary>
+    public interface IPreparedFilter 
+    {
+
+        /// <summary>
+        /// Get the unique identifier for the filter
+        /// </summary>
+        Guid Id { get; }
+
+        /// <summary>
+        /// Gets the user that created the index
+        /// </summary>
+        Guid CreatedByKey { get; }
+
+        /// <summary>
+        /// Gets the time that the index was updated
+        /// </summary>
+        DateTimeOffset? UpdatedTime { get; }
+
+        /// <summary>
+        /// Gets the user that updated the index
+        /// </summary>
+        Guid? UpdatedByKey { get; }
+
+        /// <summary>
+        /// Gets the time that the index was created
+        /// </summary>
+        DateTimeOffset CreationTime { get; }
+
+        /// <summary>
+        /// Get the name of the prepared filter index
+        /// </summary>
+        String Name { get; }
+
+        /// <summary>
+        /// Gets the type of data to which the prepared filter index applies
+        /// </summary>
+        Type TargetType { get; }
+
+        /// <summary>
+        /// The objects to be prepared
+        /// </summary>
+        Expression IndexExpression { get; }
+
+        /// <summary>
+        /// Indexing strategy provider
+        /// </summary>
+        String Indexer { get;  }
+
+        /// <summary>
+        /// Gets the status of the filter index
+        /// </summary>
+        PreparedFilterIndexState Status { get; }
+
+        /// <summary>
+        /// Last re-index
+        /// </summary>
+        DateTimeOffset? LastReindex { get; }
+    }
+}
---
+++ SanteDB.Core.Api/Data/IPreparedFilterManager.cs
@@ -0,0 +1,62 @@
+﻿using SanteDB.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace SanteDB.Core.Data
+{
+
+    /// <summary>
+    /// The prepared index filter service permits for pre-execution of filters
+    /// and storage of the results matching the filters
+    /// </summary>
+    public interface IPreparedFilterManager : IServiceImplementation
+    {
+
+        /// <summary>
+        /// Get all filter indices which are configured
+        /// </summary>
+        IEnumerable<IPreparedFilter> GetPreparedFilters();
+
+        /// <summary>
+        /// Rebuild the specified index
+        /// </summary>
+        /// <remarks>A re-build instructs the filter index manager mark the filter
+        /// prefetch index to be marked offline (don't use) and re-build the index
+        /// expression</remarks>
+        /// <param name="indexId">The filter index to be rebuilt</param>
+        IPreparedFilter ReCompute(Guid indexId);
+
+        /// <summary>
+        /// Get the specified filter index definition
+        /// </summary>
+        /// <param name="indexId">The filter for which the definition should be fetched</param>
+        /// <returns>The matching filter index</returns>
+        IPreparedFilter Get(Guid indexId);
+
+        /// <summary>
+        /// Create the specified index on <typeparamref name="TTarget"/>
+        /// </summary>
+        /// <typeparam name="TTarget">The type of object to be indexed</typeparam>
+        /// <param name="indexExpression">The expression(s) to index</param>
+        /// <param name="indexProvider">The name of the indexing provider</param>
+        /// <param name="name">The name of the filter</param>
+        IPreparedFilter Create<TTarget>(String name, Expression<Func<TTarget, dynamic>> indexExpression, String indexProvider = null);
+
+        /// <summary>
+        /// Delete the specified index
+        /// </summary>
+        /// <param name="indexId">The index identifier which is to be deleted</param>
+        /// <returns>The filter index which is being rebuilt</returns>
+        IPreparedFilter Delete(Guid indexId);
+
+        /// <summary>
+        /// Transition the state of the index 
+        /// </summary>
+        /// <param name="indexId">The index to be rebuilt</param>
+        /// <param name="newState">The new state to place the index into</param>
+        /// <returns>The prepared filter index</returns>
+        IPreparedFilter SetStatus(Guid indexId, PreparedFilterIndexState newState);
+    }
+}
