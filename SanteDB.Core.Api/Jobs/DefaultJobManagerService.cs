77cc637730acd059b70ff9d0531c68a41392a999
Justin Fyfe <justin@fyfesoftware.ca>
2022-01-26 12:12:33 -0500

    Updated the frequency of timers

--- SanteDB.Core.Api/Jobs/DefaultJobManagerService.cs
+++ SanteDB.Core.Api/Jobs/DefaultJobManagerService.cs
@@ -172,7 +172,7 @@ namespace SanteDB.Core.Jobs
             }
 
             // Setup timers based on the jobs
-            this.m_systemTimer = new System.Timers.Timer(60000); // timer runs every 5 minutes
+            this.m_systemTimer = new System.Timers.Timer(300000); // timer runs every 5 minutes
             this.m_systemTimer.Elapsed += SystemJobTimer;
             this.m_systemTimer.Enabled = true;
             this.m_systemTimer.Start();
