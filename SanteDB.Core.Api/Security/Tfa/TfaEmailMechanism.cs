fb3c0ef820772709e38a1a3c28808deba5813210
Justin Fyfe <justin@fyfesoftware.ca>
2022-09-17 16:06:30 -0400

    Heavy refactoring of the core persistence services to remove .NET Framework 4.7 code and refactor the Server plugin code out. There should be no differentiation between iCDR and dCDR services. SCOM-120

---
+++ SanteDB.Core.Api/Security/Tfa/TfaEmailMechanism.cs
@@ -0,0 +1,159 @@
+﻿/*
+ * Copyright (C) 2021 - 2022, SanteSuite Inc. and the SanteSuite Contributors (See NOTICE.md for full copyright notices)
+ * Copyright (C) 2019 - 2021, Fyfe Software Inc. and the SanteSuite Contributors
+ * Portions Copyright (C) 2015-2018 Mohawk College of Applied Arts and Technology
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * User: fyfej
+ * Date: 2022-5-30
+ */
+using SanteDB.Core.Diagnostics;
+using SanteDB.Core.Model.Entities;
+using SanteDB.Core.Model.Security;
+using SanteDB.Core.Notifications;
+using SanteDB.Core.Services;
+using System;
+using System.Diagnostics;
+using System.Diagnostics.Tracing;
+using System.Globalization;
+using System.Linq;
+using SanteDB.Core.Model;
+using SanteDB.Core.Security.Claims;
+using System.Security;
+using SanteDB.Core.Security.Services;
+using SanteDB.Core;
+using SanteDB.Core.Security;
+using System.Security.Principal;
+
+namespace SanteDB.Security.Tfa.Email
+{
+    /// <summary>
+    /// Represents a TFA mechanism which can send/receive TFA requests via e-mail
+    /// </summary>
+    public class TfaEmailMechanism : ITfaMechanism
+    {
+        private readonly ITwoFactorSecretGenerator m_twoFactorSecretGenerator;
+
+        private readonly IIdentityProviderService m_identityProvider;
+
+        private readonly IPasswordHashingService m_hashingProvider;
+
+        /// <summary>
+        /// TFA Mechanism via e-mail
+        /// </summary>
+        public TfaEmailMechanism(ITwoFactorSecretGenerator secretGenerator, IPasswordHashingService passwordHashingService, IIdentityProviderService identityProvider)
+        {
+            this.m_twoFactorSecretGenerator = secretGenerator;
+            this.m_identityProvider = identityProvider;
+            this.m_hashingProvider = passwordHashingService;
+        }
+
+        // Tracer
+        private readonly Tracer m_tracer = Tracer.GetTracer(typeof(TfaEmailMechanism));
+
+        /// <summary>
+        /// Get the identifier for the challenge
+        /// </summary>
+        public Guid Id
+        {
+            get
+            {
+                return Guid.Parse("D919457D-E015-435C-BD35-42E425E2C60C");
+            }
+        }
+
+        /// <summary>
+        /// Gets the name of the mechanism
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return "org.santedb.tfa.email";
+            }
+        }
+
+        /// <summary>
+        /// Send the mechanism
+        /// </summary>
+        public string Send(IIdentity user)
+        {
+            if (user == null)
+                throw new ArgumentNullException(nameof(user));
+
+            // Gather the e-mail
+            var filler = ApplicationServiceContext.Current.GetService<INotificationTemplateFiller>();
+            if (filler == null)
+                throw new InvalidOperationException("Cannot find notification filler service");
+
+            // We want to send the data in which language?
+            String language = CultureInfo.CurrentCulture.TwoLetterISOLanguageName;
+
+            // Generate a TFA secret and add it as a claim on the user
+            var secret = this.m_twoFactorSecretGenerator.GenerateTfaSecret();
+
+            if (user is IClaimsIdentity ci)
+            {
+                // Get user's e-mail
+                var email = ci.FindFirst(SanteDBClaimTypes.Email)?.Value;
+                if (email == null)
+                {
+                    throw new InvalidOperationException("E-Mail TFA requires e-mail address registered");
+                }
+
+                // Save secret
+                this.m_identityProvider.AddClaim(user.Name, new SanteDBClaim(SanteDBClaimTypes.SanteDBOTAuthCode, this.m_hashingProvider.ComputeHash(secret)), AuthenticationContext.SystemPrincipal, new TimeSpan(0, 5, 0));
+                // Send
+                var template = filler.FillTemplate("tfa.email", language, new
+                {
+                    user = user,
+                    tfa = secret,
+                    principal = AuthenticationContext.Current.Principal
+                });
+
+                try
+                {
+                    ApplicationServiceContext.Current.GetService<INotificationService>().Send(new string[] { email }, template.Subject, template.Body, null, false, null);
+                    var censoredEmail = email.Split('@')[1];
+                    return $"Code sent to *****@{censoredEmail}";
+                }
+                catch (Exception e)
+                {
+                    this.m_tracer.TraceEvent(EventLevel.Error, $"Error sending TFA secret: {e.Message}\r\n{e.ToString()}");
+                    throw new Exception($"Error dispatching OTP TFA code", e);
+                }
+            }
+            else
+            {
+                throw new InvalidOperationException("TFA E-mail not compatible with this mechanism");
+            }
+        }
+
+        /// <summary>
+        /// Validate the secret
+        /// </summary>
+        public bool Validate(IIdentity user, String tfaSecret)
+        {
+            if (user is IClaimsIdentity ci)
+            {
+                return ci.FindFirst(SanteDBClaimTypes.SanteDBOTAuthCode)?.Value == this.m_hashingProvider.ComputeHash(tfaSecret);
+            }
+            else
+            {
+                // TODO: When a non-CI is provided
+                return false;
+            }
+        }
+    }
+}
---
+++ SanteDB.Core.Api/ServiceUtil.cs
@@ -0,0 +1,77 @@
+﻿using SanteDB.Core.Diagnostics;
+using SanteDB.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using System.Text;
+
+namespace SanteDB.Core
+{
+    /// <summary>
+    /// Utility for starting / stopping hosted services
+    /// </summary>
+    public static class ServiceUtil
+    {
+
+        /// <summary>
+        /// Helper function to start the services
+        /// </summary>
+        public static void Start(Guid activityId, IApplicationServiceContext applicationServiceContext)
+        {
+            Trace.CorrelationManager.ActivityId = activityId;
+            Trace.TraceInformation("Starting host context on Console Presentation System at {0}", DateTime.Now);
+
+            // Do this because loading stuff is tricky ;)
+            AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(CurrentDomain_AssemblyResolve);
+
+            try
+            {
+                // Initialize 
+                ApplicationServiceContext.Current = applicationServiceContext;
+                ApplicationServiceContext.Current.Start();
+            }
+            catch (Exception e)
+            {
+                Trace.TraceError("Fatal exception occurred: {0}", e.ToString());
+                Stop();
+                throw;
+            }
+            finally
+            {
+            }
+        }
+
+        /// <summary>
+        /// Stop the service
+        /// </summary>
+        public static void Stop()
+        {
+            ApplicationServiceContext.Current?.Stop();
+            Tracer.DisposeWriters();
+        }
+
+        /// <summary>
+        /// Assembly resolution
+        /// </summary>
+        internal static Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            foreach (Assembly asm in AppDomain.CurrentDomain.GetAssemblies())
+                if (args.Name == asm.FullName)
+                    return asm;
+
+            /// Try for an non-same number Version
+            foreach (Assembly asm in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                string fAsmName = args.Name;
+                if (fAsmName.Contains(","))
+                    fAsmName = fAsmName.Substring(0, fAsmName.IndexOf(","));
+                if (fAsmName == asm.GetName().Name)
+                    return asm;
+            }
+
+            return null;
+        }
+
+    }
+}
