566b68358b0a3803dfab50da8535aeb2600e3498
Justin Fyfe <justin@fyfesoftware.ca>
2022-09-17 17:33:58 -0400

    Updated the WWW service behavior default configuration to be able to resolve default applets SCOM-120

---
+++ SanteDB.Core.Api/Configuration/Features/IFeatureFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SanteDB.Core.Configuration.Features
+{
+    /// <summary>
+    /// Represents a class which can create other features
+    /// </summary>
+    public interface IFeatureFactory
+    {
+
+        /// <summary>
+        /// Instructs the feactory to create and get features that it needs
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<IFeature> GetFeatures();
+
+    }
+}
