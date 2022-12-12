1b375f8fc928294a4d4ae17a07c6254d5fe8348b
Trevor Davis <trevor@trevordavis.me>
2022-12-12 11:03:37 -0500

    Added unit tests for base32 encoder.

---
+++ SanteDB.Core.Api.Test/Base32EncoderTests.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SanteDB.Core.Api.Test
+{
+    [TestFixture(Category ="Core API")]
+    public class Base32EncoderTests
+    {
+        [Test]
+        public void TestBase32Encode_SimpleString()
+        {
+            var input = Encoding.UTF8.GetBytes("The quick brown fox jumps over the lazy dog.");
+
+            var actual = input.Base32Encode();
+
+            Assert.AreEqual("KRUGKIDROVUWG2ZAMJZG653OEBTG66BANJ2W24DTEBXXMZLSEB2GQZJANRQXU6JAMRXWOLQ=", actual);
+        }
+
+        [Test]
+        public void TestBase32Encode_LessThan1Block()
+        {
+            var inputs = new Dictionary<string, string>()
+            {
+                { "A", "IE======" },
+                { "AB", "IFBA====" },
+                {"ABC", "IFBEG===" },
+                {"ABCD", "IFBEGRA=" }
+            };
+
+            foreach(var input in inputs)
+            {
+                var actual = Encoding.UTF8.GetBytes(input.Key).Base32Encode();
+
+                Assert.AreEqual(input.Value, actual);
+            }
+        }
+
+        [Test]
+        public void TestBase32Encode_EmptyString()
+        {
+            var input = new byte[0];
+
+            var actual = input.Base32Encode();
+
+            Assert.AreEqual("", actual);
+        }
+    }
+}
