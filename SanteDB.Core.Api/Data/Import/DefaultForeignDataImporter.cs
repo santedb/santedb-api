cf6bda992a54f9c99d277ca805c4bf060613f928
Justin Fyfe <justin@fyfesoftware.ca>
2023-01-10 13:33:22 -0500

    Updated the manner in which average record speed is calculated so that it is only performed on 60 second windows

--- SanteDB.Core.Api/Data/Import/DefaultForeignDataImporter.cs
+++ SanteDB.Core.Api/Data/Import/DefaultForeignDataImporter.cs
@@ -74,7 +74,7 @@ namespace SanteDB.Core.Data.Import
                 var parmNo = i;
                 duplicateCheckParms.Add(sourceReader.GetName(i), () => sourceReader[parmNo]);
             }
-            int records = 0;
+            int records = 0, sampleRecords = 0 ;
             var sw = new Stopwatch();
             sw.Start();
             while (sourceReader.MoveNext())
@@ -85,9 +85,15 @@ namespace SanteDB.Core.Data.Import
                 // Is there a duplicate check? If so map them
                 var duplicateChecks = foreignDataObjectMap.DuplicateCheck?.Select(o => QueryExpressionParser.BuildLinqExpression(foreignDataObjectMap.Resource.Type, o.ParseQueryString(), "o", variables: duplicateCheckParms)).ToList();
 
-                this.m_tracer.TraceInfo("Processing {0} from import...", records++);
+                this.m_tracer.TraceInfo("Processing {0} from import...", records);
 
-                this.ProgressChanged?.Invoke(this, new ProgressChangedEventArgs(0.5f, String.Format(UserMessages.IMPORTING, records, records / (((sw.ElapsedMilliseconds / 1000)+1)))));
+                this.ProgressChanged?.Invoke(this, new ProgressChangedEventArgs(0.5f, String.Format(UserMessages.IMPORTING, records++, 1000.0f * (float)(sampleRecords++) / (float)sw.ElapsedMilliseconds)));
+                if(sw.ElapsedMilliseconds > 60000)
+                {
+                    sw.Stop();
+                    sw.Restart();
+                    sampleRecords = 0;
+                }
                 if (duplicateChecks.Any())
                 {
                     mappedObject = duplicateChecks.Select(o => repositoryService.Find(o).FirstOrDefault())?.FirstOrDefault() as IdentifiedData;
