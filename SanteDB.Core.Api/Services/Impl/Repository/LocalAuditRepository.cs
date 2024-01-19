05918b3f23a9941e4795023a72ccccf3e3c4985d
Justin Fyfe <justin@fyfesoftware.ca>
2024-01-19 14:56:17 -0500

    Removed persistence notifications from teh audit repository and updated the default audit configuration settings. Added group names to constants file

--- SanteDB.Core.Api/Services/Impl/Repository/LocalAuditRepository.cs
+++ SanteDB.Core.Api/Services/Impl/Repository/LocalAuditRepository.cs
@@ -34,7 +34,7 @@ namespace SanteDB.Core.Services.Impl.Repository
     /// Represents an audit repository which stores and queries audit data.
     /// </summary>
     [ServiceProvider("Default Audit Repository")]
-    public class LocalAuditRepository : IRepositoryService<AuditEventData>, INotifyRepositoryService<AuditEventData>
+    public class LocalAuditRepository : IRepositoryService<AuditEventData>
     {
         // Localization Service
         private readonly ILocalizationService m_localizationService;
@@ -45,27 +45,6 @@ namespace SanteDB.Core.Services.Impl.Repository
         // Persistence service
         private readonly IDataPersistenceService<AuditEventData> m_persistenceService;
         private readonly IPolicyEnforcementService m_pepService;
-
-        /// <inheritdoc/>
-        public event EventHandler<DataPersistingEventArgs<AuditEventData>> Inserting;
-        /// <inheritdoc/>
-        public event EventHandler<DataPersistedEventArgs<AuditEventData>> Inserted;
-        /// <inheritdoc/>
-        public event EventHandler<DataPersistingEventArgs<AuditEventData>> Saving;
-        /// <inheritdoc/>
-        public event EventHandler<DataPersistedEventArgs<AuditEventData>> Saved;
-        /// <inheritdoc/>
-        public event EventHandler<DataPersistingEventArgs<AuditEventData>> Deleting;
-        /// <inheritdoc/>
-        public event EventHandler<DataPersistedEventArgs<AuditEventData>> Deleted;
-        /// <inheritdoc/>
-        public event EventHandler<DataRetrievingEventArgs<AuditEventData>> Retrieving;
-        /// <inheritdoc/>
-        public event EventHandler<DataRetrievedEventArgs<AuditEventData>> Retrieved;
-        /// <inheritdoc/>
-        public event EventHandler<QueryRequestEventArgs<AuditEventData>> Querying;
-        /// <inheritdoc/>
-        public event EventHandler<QueryResultEventArgs<AuditEventData>> Queried;
 
         /// <summary>
         /// Construct instance of LocalAuditRepository
@@ -154,18 +133,8 @@ namespace SanteDB.Core.Services.Impl.Repository
         public AuditEventData Insert(AuditEventData audit)
         {
             var preArgs = new DataPersistingEventArgs<AuditEventData>(audit, TransactionMode.Commit, AuthenticationContext.Current.Principal);
-            this.Inserting?.Invoke(this, preArgs);
-            if (!preArgs.Cancel)
-            {
-                var retVal = this.m_persistenceService?.Insert(audit, TransactionMode.Commit, AuthenticationContext.Current.Principal);
-                this.Inserted?.Invoke(this, new DataPersistedEventArgs<AuditEventData>(retVal, TransactionMode.Commit, AuthenticationContext.Current.Principal));
-                return retVal;
-            }
-            else
-            {
-                this.m_tracer.TraceWarning("Pre-Event arg signals cancel on insertion of audit");
-                return preArgs.Data;
-            }
+            var retVal = this.m_persistenceService?.Insert(audit, TransactionMode.Commit, AuthenticationContext.Current.Principal);
+            return retVal;
         }
 
         /// <summary>
@@ -174,19 +143,8 @@ namespace SanteDB.Core.Services.Impl.Repository
         public AuditEventData Delete(Guid key)
         {
             this.m_pepService.Demand(PermissionPolicyIdentifiers.AccessAuditLog);
-            var preArgs = new DataPersistingEventArgs<AuditEventData>(new AuditEventData() {  Key = key }, TransactionMode.Commit, AuthenticationContext.Current.Principal);
-            this.Deleting?.Invoke(this, preArgs);
-            if (!preArgs.Cancel)
-            {
-                var retVal = this.m_persistenceService?.Delete(key, TransactionMode.Commit, AuthenticationContext.Current.Principal);
-                this.Deleted?.Invoke(this, new DataPersistedEventArgs<AuditEventData>(retVal, TransactionMode.Commit, AuthenticationContext.Current.Principal));
-                return retVal;
-            }
-            else
-            {
-                this.m_tracer.TraceWarning("Pre-Event arg signals cancel on deletion of audit");
-                return preArgs.Data;
-            }
+            var retVal = this.m_persistenceService?.Delete(key, TransactionMode.Commit, AuthenticationContext.Current.Principal);
+            return retVal;
         }
 
         /// <summary>
@@ -197,28 +155,18 @@ namespace SanteDB.Core.Services.Impl.Repository
             this.m_pepService.Demand(PermissionPolicyIdentifiers.AccessAuditLog);
 
             var preArgs = new DataPersistingEventArgs<AuditEventData>(data, TransactionMode.Commit, AuthenticationContext.Current.Principal);
-            this.Saving?.Invoke(this, preArgs);
 
-            if (!preArgs.Cancel)
+            var existing = this.m_persistenceService.Get(data.Key.Value, null, AuthenticationContext.Current.Principal);
+            AuditEventData retVal = null;
+            if (existing == null)
             {
-                var existing = this.m_persistenceService.Get(data.Key.Value, null, AuthenticationContext.Current.Principal);
-                AuditEventData retVal = null;
-                if (existing == null)
-                {
-                    retVal = this.m_persistenceService?.Update(data, TransactionMode.Commit, AuthenticationContext.Current.Principal);
-                }
-                else
-                {
-                    retVal = this.m_persistenceService?.Insert(data, TransactionMode.Commit, AuthenticationContext.Current.Principal);
-                }
-                this.Saved?.Invoke(this, new DataPersistedEventArgs<AuditEventData>(retVal, TransactionMode.Commit, AuthenticationContext.Current.Principal)); ;
-                return retVal;
+                retVal = this.m_persistenceService?.Update(data, TransactionMode.Commit, AuthenticationContext.Current.Principal);
             }
             else
             {
-                this.m_tracer.TraceWarning("Pre-Event arg signals cancel on save of audit");
-                return preArgs.Data;
+                retVal = this.m_persistenceService?.Insert(data, TransactionMode.Commit, AuthenticationContext.Current.Principal);
             }
+            return retVal;
         }
     }
 }
