6e7fb87b3a8153dccd4cc21e3acf6cb81d4065f5
Justin Fyfe <justin@fyfesoftware.ca>
2024-09-11 11:14:25 -0400

    Updated the dataset source to accept a configuration section in the configuration file (makes it easier for projects to initializae their environment)

---
+++ SanteDB.Core.Api/Configuration/FileSystemDatasetProviderConfigurationSection.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace SanteDB.Core.Configuration
+{
+    /// <summary>
+    /// File system dataset provider configuration section
+    /// </summary>
+    [XmlType(nameof(FileSystemDatasetProviderConfigurationSection), Namespace = "http://santedb.org/configuration")]
+    public class FileSystemDatasetProviderConfigurationSection : IConfigurationSection
+    {
+
+
+        /// <summary>
+        /// Gets or sets the sources of the dataset processing
+        /// </summary>
+        [XmlArray("sources"), XmlArrayItem("add"), JsonProperty("sources")]
+        public List<String> Sources { get; set; }
+
+    }
+}
