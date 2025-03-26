8fe2eab51461948bf42d46f9934efa83f55081a0
Jordan Webber <webberjo@hhsc.ca>
2025-03-26 17:07:27 -0400

    JIMS-749 Created model for notifications

---
+++ SanteDB.Core.Api/Notifications/Notification.cs
@@ -0,0 +1,114 @@
+﻿/*
+ * Copyright (C) 2021 - 2024, SanteSuite Inc. and the SanteSuite Contributors (See NOTICE.md for full copyright notices)
+ * Copyright (C) 2019 - 2021, Fyfe Software Inc. and the SanteSuite Contributors
+ * Portions Copyright (C) 2015-2018 Mohawk College of Applied Arts and Technology
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you 
+ * may not use this file except in compliance with the License. You may 
+ * obtain a copy of the License at 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
+ * License for the specific language governing permissions and limitations under 
+ * the License.
+ * 
+ */
+using System;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+using SanteDB.Core.Model.DataTypes;
+using SanteDB.Core.Model.Entities;
+using SanteDB.Core.Model.Interfaces;
+
+namespace SanteDB.Core.Notifications
+{
+    // <summary>
+    /// Represents a notification resource
+    /// </summary>
+    [XmlType(nameof(Notification), Namespace = "http://santedb.org/notification")]
+    [XmlRoot(nameof(Notification), Namespace = "http://santedb.org/notification")]
+    [JsonObject]
+    public class Notification : IIdentifiedResource
+    {
+        /// <summary>
+        /// Gets or set the identifier
+        /// </summary>
+        [XmlAttribute("id"), JsonProperty("id")]
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the template
+        /// </summary>
+        [XmlAttribute("template"), JsonProperty("template")]
+        public NotificationTemplate Template { get; set; }
+
+        /// <summary>
+        /// Gets or sets the entity type for notification
+        /// </summary>
+        [XmlAttribute("entityType"), JsonProperty("entityType")]
+        public Entity EntityType { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the notification
+        /// </summary>
+        [XmlAttribute("name"), JsonProperty("name")]
+        public String Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the mnemonic of the notification
+        /// </summary>
+        [XmlAttribute("mnemonic"), JsonProperty("mnemonic")]
+        public String Mnemonic { get; set; }
+
+        /// <summary>
+        /// Gets or sets the state of the notification
+        /// </summary>
+        [XmlAttribute("state"), JsonProperty("state")]
+        public Concept State { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description of the notification
+        /// </summary>
+        [XmlAttribute("description"), JsonProperty("description")]
+        public String Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the filter for the notification
+        /// </summary>
+        [XmlAttribute("filter"), JsonProperty("filter")]
+        public String Filter { get; set; }
+
+        /// <summary>
+        /// Gets or sets the trigger for the notification
+        /// </summary>
+        [XmlAttribute("trigger"), JsonProperty("trigger")]
+        public String Trigger { get; set; }
+
+        /// <summary>
+        /// Gets or sets the trigger for the notification
+        /// </summary>
+        [XmlAttribute("target"), JsonProperty("target")]
+        public String Target { get; set; }
+
+        /// <summary>
+        /// Gets or sets the key of the notification
+        /// </summary>
+        [XmlAttribute("key"), JsonProperty("key")]
+        public Guid? Key { get; set; }
+
+        /// <summary>
+        /// Gets or sets the tag of the notification
+        /// </summary>
+        [XmlAttribute("tag"), JsonProperty("tag")]
+        public string Tag { get; }
+
+        /// <summary>
+        /// Gets or sets the modified on datetime of the notification
+        /// </summary>
+        [XmlAttribute("modifiedOn"), JsonProperty("modifiedOn")]
+        public DateTimeOffset ModifiedOn { get; }
+    }
+}
