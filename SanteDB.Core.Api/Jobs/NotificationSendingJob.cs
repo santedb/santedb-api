8c744d66e52f9823d4bd661065c38e19f67ca644
Ryan Hull <hullr@HHSC.ca>
2025-05-14 15:50:17 -0400

    Added a job for sending scheduled notifications

---
+++ SanteDB.Core.Api/Jobs/NotificationSendingJob.cs
@@ -0,0 +1,123 @@
+﻿/*
+ * Copyright (C) 2021 - 2024, SanteSuite Inc. and the SanteSuite Contributors (See NOTICE.md for full copyright notices)
+ * Copyright (C) 2019 - 2021, Fyfe Software Inc. and the SanteSuite Contributors
+ * Portions Copyright (C) 2015-2018 Mohawk College of Applied Arts and Technology
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you 
+ * may not use this file except in compliance with the License. You may 
+ * obtain a copy of the License at 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
+ * License for the specific language governing permissions and limitations under 
+ * the License.
+ * 
+ */
+using SanteDB.Core.Model.Constants;
+using SanteDB.Core.Model.Query;
+using SanteDB.Core.Notifications;
+using SanteDB.Core.Services;
+using SharpCompress;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SanteDB.Core.Jobs
+{
+    /// <summary>
+    /// </summary>
+    public class NotificationSendingJob : IJob
+    {
+        private readonly IJobManagerService m_jobManager;
+
+        private readonly IJobStateManagerService m_jobStateManager;
+
+        private readonly IRepositoryService<NotificationInstance> m_repositoryService;
+
+        private bool m_cancelRequested = false;
+
+        /// <summary>
+        /// Job Id
+        /// </summary>
+        public static readonly Guid JOB_ID = Guid.Parse("A5C97883-A21E-4C33-B428-E69002B7A453");
+
+        /// <inheritdoc/>
+        public Guid Id => JOB_ID;
+
+        /// <inheritdoc/>
+        public string Name => "Send Scheduled Notifications Job";
+
+        /// <inheritdoc/>
+        public string Description => "Sends any due scheduled notifications";
+
+        /// <inheritdoc/>
+        public bool CanCancel => true;
+
+        /// <inheritdoc/>
+        public IDictionary<string, Type> Parameters => null;
+
+        /// <inheritdoc/>
+        public DateTime? LastStarted { get; private set; }
+
+        /// <inheritdoc/>
+        public DateTime? LastFinished { get; private set; }
+
+        /// <summary>
+        /// Dependency injected constructor
+        /// </summary>
+        public NotificationSendingJob(IJobStateManagerService jobStateManagerService, IJobManagerService jobManagerService, IRepositoryService<NotificationInstance> repositoryService)
+        {
+            this.m_jobManager = jobManagerService;
+            this.m_jobStateManager = jobStateManagerService;
+            this.m_repositoryService = repositoryService;
+        }
+
+        /// <inheritdoc/>
+        public void Cancel()
+        {
+            this.m_cancelRequested = true;
+            this.m_jobStateManager.SetState(this, JobStateType.Cancelled);
+
+        }
+
+        /// <inheritdoc/>
+        public void Run(object sender, EventArgs e, object[] parameters)
+        {
+            try
+            {
+                this.m_jobStateManager.SetState(this, JobStateType.Running);
+                this.m_cancelRequested = false;
+                this.LastStarted = DateTime.Now;
+
+                var enabledNotifications = this.m_repositoryService.Find(i => i.StateKey != Guid.Parse("1E029E45-734E-4514-9CA4-E1E487883562")).ToArray();
+  
+                enabledNotifications.ForEach(notification =>
+                {
+                    // Send if notification is due
+                    if (!this.m_cancelRequested)
+                    {
+                        Console.WriteLine("Notification Sent");
+                    }
+                });
+
+                if (this.m_cancelRequested)
+                {
+                    this.m_jobStateManager.SetState(this, JobStateType.Cancelled);
+                }
+                else
+                {
+                    this.m_jobStateManager.SetState(this, JobStateType.Completed);
+                }
+
+                this.LastFinished = DateTime.Now;
+            }
+            catch (Exception ex)
+            {
+                this.m_jobStateManager.SetState(this, JobStateType.Aborted, ex.ToHumanReadableString());
+            }
+        }
+    }
+}
