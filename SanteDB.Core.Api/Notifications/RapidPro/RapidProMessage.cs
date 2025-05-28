6546534fb102d1cdc5eb31d091f3fd2e54dd7a5b
Jordan Webber <webberjo@hhsc.ca>
2025-05-28 16:00:52 -0400

    Created model for a RapidPro message.

---
+++ SanteDB.Core.Api/Notifications/RapidPro/RapidProMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace SanteDB.Core.Notifications.RapidPro
+{
+    public class RapidProMessage
+    {
+        [JsonProperty("contact")]
+        public string Contact { get; set; }
+        [JsonProperty("text")]
+        public string Text { get; set; }
+    }
+}
