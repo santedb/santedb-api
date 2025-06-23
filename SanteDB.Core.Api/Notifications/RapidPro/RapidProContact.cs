25ad3b175a58b90b2481cf1e8b65e95a86c37716
Daria <49134543+dariaost@users.noreply.github.com>
2025-06-23 16:06:59 -0400

    JIMS-680 - Scheduled notifications (#7)
    * JIMS-749 Created model for notifications
    
    * JIMS-757 Created the application model for Parameters
    
    * JIMS-758 Created the application model for ParameterValues
    
    * JIMS-749 Renamed the Notification model and updated it to derive from NonVersionedEntityData
    
    * Updated data type in NotificationInstanceParameter and updated NotificationInstance to use inherited properties
    
    * Updated data attributes in application models used for scheduled notifications
    
    * Updated NotificationTemplate properties
    
    * Updated the application models for NotificationInstance and NotificationInstanceParameter
    
    * Updated application models for scheduled notifications
    
    * JIMS-758 Updated the application model for NotificationInstanceParameter to inherit from NonVersionedEntityData, implement ISimpleAssociation, updated xml/json field names, and removed Id property
    
    * JIMS-749 Updated xml/json fields and removed Id property from the application model for NotificationInstance
    
    * JIMS-757, JIMS-760 Updated the information models for notification template contents and parameters
    
    * Updated the XmlElement and JsonProperty of the StatusKey in NotificationTemplate
    
    * Removed Id property from NotificationTemplate
    
    * Changed the xml and json template key name.
    
    * Added LastSentAt and LastSentAtXml properties for NotificationInstance's application model
    
    * Update Notification Instance model to change the json and xml property for template.
    
    * Added a job for sending scheduled notifications
    
    * Add a sample connection to RapidPRO and add authentication into the request
    
    * Updated NotificationSendingJob logic to use each instance's trigger and filter expressions
    
    * Fixed variable casing and added code to update each due NotificatioInstance's LastSentAt value
    
    * Updated repository name for clarity and added code to save updated NotificationInstances
    
    * RapidPro Integration
    
    * Updated NotificationSendingJob's Run method to support multiple entity types
    
    * Completed Populating template functionality
    
    * Changed DateTime to DateTimeOffset in NotificationSendingJob
    
    * Added model for the RapidPro contact
    
    * Code clean up for the models for the scheduled notifications
    
    * Replace hardcoded strings with constants in NotificationJob
    
    * Added comment explaining the hack implemented to support multiple entity types in NotificationSendingJob
    
    * Added second Try Catch in NotificationSendingJob to ensure remaining messages are still handled after an exception from PostAsync
    
    * Update PostAsync to correctly retrieve contacts
    
    * Created model for a RapidPro message.
    
    * Added documentation for RapidProMessage model.
    
    * Clean up Notification Filler to remove unused code
    
    * Code cleanup
    
    * Added RapidPro Notification Relay and configuration model.
    
    * Updated documentation for the RapidProNotificationRelay
    
    * Updated documentation for application models for scheduled notifications
    
    * Updated the documentation for the RapidProNotificationConfigurationSection model.
    
    * Add type to configuration schema for RapidProNotificationConfigurationSection.
    
    * Updated logic within NotificationSendingJob to use the MapUtil.GetModelTypeFromClassKey method
    
    * Replace template parameter reference with parameter name instead.
    
    * Line ending updates.
    
    ---------
    
    Co-authored-by: Jordan Webber <webberjo@hhsc.ca>
    Co-authored-by: Ryan Hull <hullr@HHSC.ca>
    Co-authored-by: Trevor Davis <ThreeSevenths@users.noreply.github.com>
    Co-authored-by: Trevor Davis <trevor@santesuite.com>

---
+++ SanteDB.Core.Api/Notifications/RapidPro/RapidProContact.cs
@@ -0,0 +1,66 @@
+﻿/*
+ * Copyright (C) 2021 - 2025, SanteSuite Inc. and the SanteSuite Contributors (See NOTICE.md for full copyright notices)
+ * Copyright (C) 2019 - 2021, Fyfe Software Inc. and the SanteSuite Contributors
+ * Portions Copyright (C) 2015-2018 Mohawk College of Applied Arts and Technology
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you 
+ * may not use this file except in compliance with the License. You may 
+ * obtain a copy of the License at 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
+ * License for the specific language governing permissions and limitations under 
+ * the License.
+ * 
+ */
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace SanteDB.Core.Notifications.RapidPro
+{
+    /// <summary>
+    /// Represents a contact in RapidPro
+    /// </summary>
+    public class RapidProContact
+    {
+        /// <summary>
+        /// Gets or sets the name
+        /// </summary>
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the language
+        /// </summary>
+        [JsonProperty("language")]
+        public string Language { get; set; }
+
+        /// <summary>
+        /// Gets or sets the phone number
+        /// </summary>
+        [JsonProperty("phone")]
+        public string Phone { get; set; }
+
+        /// <summary>
+        /// Gets or sets the email address
+        /// </summary>
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Gets or sets the uuid
+        /// </summary>
+        [JsonProperty("uuid")]
+        public Guid Uuid { get; set; }
+
+        /// <summary>
+        /// Gets or sets the urns
+        /// </summary>
+        [JsonProperty("urns")]
+        public List<string> Urns { get; set; }
+    }
+}
