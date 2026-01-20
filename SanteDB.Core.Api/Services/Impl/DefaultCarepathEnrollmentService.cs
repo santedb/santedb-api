499eaf07541672d52a5bd04ac166a16553e9d1ec
Justin Fyfe <justin@fyfesoftware.ca>
2026-01-20 17:21:33 -0500

    Updated the enrolment to skip if patient hasn't changed

--- SanteDB.Core.Api/Services/Impl/DefaultCarepathEnrollmentService.cs
+++ SanteDB.Core.Api/Services/Impl/DefaultCarepathEnrollmentService.cs
@@ -144,12 +144,29 @@ namespace SanteDB.Core.Services.Impl
                     }));
                 p.SetLoaded(o => o.Participations);
                 var eligibleCarePaths = this.GetEligibleCarePaths(p).ToList();
+
                 // All enrolled carepaths where the person is no longer eligible
-                foreach (var er in this.GetEnrolledCarePaths(p))
+                if (p.BatchOperation == Model.DataTypes.BatchOperationType.Update ||
+                    p.BatchOperation == Model.DataTypes.BatchOperationType.InsertOrUpdate ||
+                    p.BatchOperation == Model.DataTypes.BatchOperationType.Auto)
                 {
-                    if (!eligibleCarePaths.Any(cp => cp.Key == er.Key))
+                    var enrolledCarePaths = this.GetEnrolledCarePaths(p).ToList();
+                    foreach (var er in enrolledCarePaths) // Remove the patient from any enrolled carepaths
                     {
-                        this.UnEnroll(p, er);
+                        if (!eligibleCarePaths.Any(cp => cp.Key == er.Key))
+                        {
+                            this.UnEnroll(p, er);
+                        }
+                    }
+
+                    // Did anything change that might impact a care plan - this is gender or date of birth
+                    var existingPatient = this.m_patientRepository.Get(p.Key.Value);
+                    if(existingPatient?.DateOfBirth?.Date == p.DateOfBirth?.Date &&
+                        existingPatient?.GenderConceptKey == p.GenderConceptKey &&
+                        eligibleCarePaths.All(cp => enrolledCarePaths.Any(ep => ep.Key == cp.Key) && cp.EnrollmentMode == CarePathwayEnrollmentMode.Automatic)
+                        ) // no change
+                    {
+                        continue;
                     }
                 }
 
