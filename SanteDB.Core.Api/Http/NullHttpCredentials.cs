98a8824bbb036afea5a0434d38bd5b43d896396c
Justin Fyfe <justin@fyfesoftware.ca>
2026-02-12 15:54:05 -0500

    Added null credentials which can be used when a caller explicitly wants to bypass auth

---
+++ SanteDB.Core.Api/Http/NullHttpCredentials.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Security.Principal;
+using System.Text;
+
+namespace SanteDB.Core.Http
+{
+    /// <summary>
+    /// Request credentials which are NULL
+    /// </summary>
+    public class NullHttpCredentials : RestRequestCredentials
+    {
+        /// <inheritdoc/>
+        public NullHttpCredentials(IPrincipal principal) : base(principal)
+        {
+        }
+
+        /// <inheritdoc/>
+        public override void SetCredentials(HttpWebRequest webRequest)
+        {
+        }
+    }
+}
